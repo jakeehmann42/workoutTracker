10bf77e7e4f3ef9e5f98f8d786960731e3e21fc2
Jakeehmann42 <Jakeehmann42@gmail.com>
2020-12-22 11:21:59 -0700

    deployed?

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require("express");
+
+const logger = require("morgan");
+
+const mongoose = require("mongoose");
+
+const path = require("path");
+
+const apiRoutes = require("./routes/apiRoutes.js");
+
+
+
+
+const PORT = process.env.PORT || 3000;
+
+const db = require("./models");
+
+const app = express();
+
+
+
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/WorkoutLogger", { useNewUrlParser: true });
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/exercise.html"));
+});
+
+app.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/stats.html"));
+});
+
+app.use(apiRoutes);
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+});
